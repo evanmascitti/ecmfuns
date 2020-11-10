2065f9a2d4dde060cf60ea96591b0cebc047c7f7
‘evanmascitti’ <evanmascitti@gmail.com>
2020-11-10 15:50:06 -0500

    added fsize function to easily read file sizes

---
+++ R/fsize.R
@@ -0,0 +1,14 @@
+#' Print a file size in a human-readable format
+#'
+#' @param path quoted path to file
+#' @param digits number of decimal places to pring
+#'
+#' @return The file size, formatted with appropriate prefix
+#' @export
+#'
+#' @seealso \link[base]{file.size}, \link[gdata]{humanReadable}
+#'
+image_filesize <- function(path, digits = 0){
+
+  gdata::humanReadable(x = path, standard = "SI", digits = digits)
+}
