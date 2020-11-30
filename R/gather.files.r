4012f328904c0ff350dbc781a5146ef8cb1f62eb
Evan C. Mascitti <ecm200@psu.edu>
2020-11-30 08:49:21 -0500

    changed gather.files function to allow option for reading in and enframing files, and accepting file extensions with preceding dot.

--- R/gather.files.r
+++ R/gather.files.r
@@ -2,24 +2,20 @@
 #'
 #'Prepare a set of files to be iteratively read into R
 #'
-#'This is a helper function to remove friction when reading multiple data files
-#'in one step. Using `list.files()` is not intuitive for reading files because it
-#'returns only the base file name and none of the parent directories. A standard
-#'method would require a `paste()` or `paste0()` call to concatenate the root path
-#'and the base file name.
+#'This is a helper function to remove friction and reduce eyesore when reading multiple data files
+#'in one step. A base
+#'method with `list.files()` works fine but requires more arguments than I want to remember for this very common task.
+#' This wrapper cleans the path strings to assign nice names to the files as they are imported.
 #'
-#'`gather.files()` reduces opacity and eyesore during this common task. It
-#'leverages the functionality of `list.files()` with the `str_sub()` function from
-#'**stringr** to remove both the parent directory and the file
-#'extensions from the names assigned upon import.
+#'@param dir Relative path to directory where the files reside. For .Rmd files
+#'  this begins at the same level as the .Rmd file
+#'@param type a quoted file extension, accepts 3-letter extension with or without a preceding "." Verify capitilization.
+#'@param read_fn unquoted function for reading files (no parentheses), for example `read_csv`
+#'@param enframe whether to actually read the files into the current R session and compile them with `tibble::enframe()`
+#'@param value column title for the files, if using `enframe` argument
+#'@param recursive whether to search sub-directories
 #'
-#'@param tabbed_directory The quoted directory in which the files reside, as
-#'  understood from the auto-complete hosted in R Studio. For .Rmd files this is
-#'  a relative path from the directory housing the .Rmd file.
-#'@param filetype a quoted file extension, \strong{without} the "." Be
-#'  sure to use correct capitalization for the file type of interest.
-#'
-#'@details This function is designed to be used with the pipe (`%>%`) from
+#' @details This function is designed to be used with the pipe (`%>%`) from
 #'  \strong{magrittr} and two additional functions: \itemize{ \item{`map()` in the
 #'  \strong{purrr} package, which allows the read-in operation to be performed
 #'  for all files in one step} \item{a user-supplied function, such as
@@ -41,25 +37,38 @@
 #'
 #' @examples \dontrun{
 #' gather.files(
-#' tabbed_directory= "data/raw_data/2020-10-25_Atterberg_limits",
-#' filetype= "csv") %>%
+#' dir = "data/raw_data/2020-10-25_Atterberg_limits",
+#' type= "csv") %>%
 #' map(read_csv) %>%
 #' enframe(value= "raw_data_file") }
 
-gather.files <- function(tabbed_directory = " ", filetype = " ") {
+gather.files <- function(dir = " ", type = " ", read_fn = NULL, enframe= TRUE, value = "file", recursive = FALSE) {
 
-  parent_dirs <- paste0(here::here(tabbed_directory), '/')
-  file_names <- list.files(path= parent_dirs, pattern = filetype, recursive = F)
-  full_paths <- paste0(parent_dirs, file_names ) %>%
-    purrr::set_names(stringr::str_remove(basename(.), pattern = paste0(".", filetype)) )
-
-  if (length(stringr::str_detect(string = file_names, pattern = filetype) ) == 0 ) {
+  if (length(stringr::str_detect(string = file_names, pattern = type) ) == 0 ) {
     stop("\n\nDirectory does not contain any files with this extension.
       Check the following:\n\n 1.  Did you specify the correct sub-folder?
-      This function does not search recursively.\n\n 2.  Did you type the
-      file extension correctly? This includes capitilization! Don't use
-      a \".\" before the 3-letter extension.")
+      By default, this function does not search recursively.\n\n 2.  Did you type the
+      file extension correctly? This includes capitilization!")
   }
-  return(full_paths)
+
+  parent_dirs <- paste0(here::here(dir), '/')
+
+  if(stringr::str_detect(type, pattern = ".")) {
+    extension <- type } else {
+      extension <- paste0(".", type)}
+
+  file_names <- list.files(path= parent_dirs, pattern = extension, recursive = recursive)
+
+
+  full_paths <- paste0(parent_dirs, file_names ) %>%
+    purrr::set_names(stringr::str_remove(basename(.), pattern = extension) )
+
+  if(enframe){
+files_tibble <- full_paths %>%
+  purrr::map({{readfn}}) %>%
+  tibble::enframe(value = value)
+  }
+
+  return(if(enframe){full_paths} else{files_tibble})
 
 }
