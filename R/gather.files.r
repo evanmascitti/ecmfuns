1d2d0e11cb69ffea1f071bf4da636d32f1aaa577
Evan C. Mascitti <ecm200@psu.edu>
2020-11-30 10:12:54 -0500

    repaired gather.files after making a mess of it and added read.files to compile the reading and enframing into one step. Added error message us if user forgets to supply readin function

--- R/gather.files.r
+++ R/gather.files.r
@@ -2,73 +2,59 @@
 #'
 #'Prepare a set of files to be iteratively read into R
 #'
-#'This is a helper function to remove friction and reduce eyesore when reading multiple data files
-#'in one step. A base
-#'method with `list.files()` works fine but requires more arguments than I want to remember for this very common task.
-#' This wrapper cleans the path strings to assign nice names to the files as they are imported.
+#'This is a helper function to remove friction and reduce eyesore when reading
+#'multiple data files in one step. A base method with `list.files()` works fine
+#'but requires more arguments than I want to remember for this very common task.
+#'This wrapper cleans the path strings to assign nice names to the files as they
+#'are imported.
 #'
-#'@param dir Relative path to directory where the files reside. For .Rmd files
-#'  this begins at the same level as the .Rmd file
-#'@param type a quoted file extension, accepts 3-letter extension with or without a preceding "." Verify capitilization.
-#'@param read_fn unquoted function for reading files (no parentheses), for example `read_csv`
-#'@param enframe whether to actually read the files into the current R session and compile them with `tibble::enframe()`
-#'@param value column title for the files, if using `enframe` argument
+#'@param dir Relative path to directory where the files reside (includes the
+#'  trailing `/`). For .Rmd files this begins at the same level as the .Rmd
+#'  file, which is usually good but can be annoying when the file you need to
+#'  read resides above the .Rmd file in the directory structure. In this case
+#'  you can look one directory upward with `/..` or to the top-level directory with
+#'  `/`.
+#'@param extension quoted file extension, accepts 3-letter extension with OR
+#'  without a preceding "." Verify capitalization.
 #'@param recursive whether to search sub-directories
 #'
-#' @details This function is designed to be used with the pipe (`%>%`) from
-#'  \strong{magrittr} and two additional functions: \itemize{ \item{`map()` in the
-#'  \strong{purrr} package, which allows the read-in operation to be performed
-#'  for all files in one step} \item{a user-supplied function, such as
-#'  `read_csv()` for comma-delimited value files or `vcgImport()` for 3D mesh
-#'  files} .}
+#'@details Output is easily piped to [`read.files()`] to read the files and
+#'  compile them in a tibble. Because `gather.files` gives nice names to its
+#'  elements, the list is more human-readable.
 #'
-#'  A recommended step to complete this pipeline is to call
-#'  `tibble::enframe()` after the `map(<reading function>)` call. This condenses
-#'  the list to a nested tibble, with each file as a row. Optionally, use the
-#'  `value` argument inside the call to `enframe()` to add a useful description
-#'  to what the file (i.e. value) actually is. Because `gather.files` provides
-#'  human-friendly names to its elements, there should not be a need to adjust
-#'  the `name` argument inside `enframe()`.
-#'
-#'@import here
 #'@return A named character vector of file paths to read. The element names are
-#'  the file name with the directories and file extension stripped.
+#'  the base file names with the file extension stripped.
 #'@export
 #'
-#' @examples \dontrun{
-#' gather.files(
-#' dir = "data/raw_data/2020-10-25_Atterberg_limits",
-#' type= "csv") %>%
-#' map(read_csv) %>%
-#' enframe(value= "raw_data_file") }
+#' @examples  \dontrun{
+#' gather.files(dir = "data/raw_data/2020-10-25_Atterberg_limits",
+#' extension= ".csv")}
+#'
+#'
 
-gather.files <- function(dir = " ", type = " ", read_fn = NULL, enframe= TRUE, value = "file", recursive = FALSE) {
+gather.files <- function(dir = ".", extension = NULL, recursive = FALSE) {
 
-  if (length(stringr::str_detect(string = file_names, pattern = type) ) == 0 ) {
+  if(missing(extension)){
+    stop('\nNo file extension provided, please specify the type of file to find.')
+  }
+
+  # if file name has the dot, leave it alone. If not, paste a dot and the three letters.
+  if(stringr::str_detect(extension, pattern = ".")) {
+    ext <- extension } else {
+      ext <- paste0(".", extension)}
+
+  full_paths <- list.files(path= dir, pattern = ext, recursive = recursive)
+
+  if (length(stringr::str_detect(string = full_paths, pattern = ext) ) == 0 ) {
     stop("\n\nDirectory does not contain any files with this extension.
       Check the following:\n\n 1.  Did you specify the correct sub-folder?
       By default, this function does not search recursively.\n\n 2.  Did you type the
-      file extension correctly? This includes capitilization!")
+      file extension correctly? Check capitalization and remember to use quotes.")
   }
 
-  parent_dirs <- paste0(here::here(dir), '/')
-
-  if(stringr::str_detect(type, pattern = ".")) {
-    extension <- type } else {
-      extension <- paste0(".", type)}
-
-  file_names <- list.files(path= parent_dirs, pattern = extension, recursive = recursive)
+  full_paths %>%
+    purrr::set_names(stringr::str_remove(string = basename(.), pattern = ext) )
 
 
-  full_paths <- paste0(parent_dirs, file_names ) %>%
-    purrr::set_names(stringr::str_remove(basename(.), pattern = extension) )
-
-  if(enframe){
-files_tibble <- full_paths %>%
-  purrr::map({{readfn}}) %>%
-  tibble::enframe(value = value)
-  }
-
-  return(if(enframe){full_paths} else{files_tibble})
-
+  return(full_paths)
 }
---
+++ R/read.files.R
@@ -0,0 +1,31 @@
+#' Read multiple files into R
+#'
+#' Wraps `purrr::map()` with an option to specify whether the list of files `.l`
+#' should be compiled with [`tibble::enframe()`]. Use after [`gather.files()`] to
+#' pipe in a vector of file paths with human-readable names.
+#'
+#' @param .x a vector of file paths to read
+#' @param read_fn un-quoted file reading function (specific to the file type; i.e. `read_csv` or `vcgImport`)
+#' @param enframe whether to create a tibble with a name column and a list-column of the files
+#' @param value column name for the files held (ignored if `enframe = FALSE`)
+#'
+#' @return a list or tibble of the files. Use [`gather.files()`] to give them nice name before importing.
+#' @export
+#'
+
+read.files <- function(.x = NULL, read_fn = NULL, enframe= TRUE, value = "file") {
+
+  if(missing(read_fun)){
+    stop('\n No `read_fn` provided. Please specify an unquoted data import function such as `read_csv`.')
+  }
+
+  if(enframe == FALSE){
+    files <- purrr::map(.x = .x, .f= {{read_fn}})
+  } else{
+    files <- purrr::map(.x = .x, .f= {{read_fn}}) %>%
+      tibble::enframe(value= value)
+  }
+
+  return(files)
+
+}
