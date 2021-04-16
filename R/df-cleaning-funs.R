c75b1d465c0ddb115111dfc2d7f9a0d60c90f73c
Evan C. Mascitti <evanmascitti@gmail.com>
2021-04-16 16:42:55 -0400

    updated figure export fn to also send to presentations/presentation-figs directory

--- R/df-cleaning-funs.R
+++ R/df-cleaning-funs.R
@@ -1,55 +1,55 @@
-#' Remove empty rows
+#' #' Remove empty rows
+#' #'
+#' #' @param df a data frame
+#' #'
+#' #' @return a data frame
+#' strip_empty_rows <- function(df){
+#'   df %>%
+#'     dplyr::filter(dplyr::if_all(.fns = ~!is.na(.)))
+#' }
 #'
-#' @param df a data frame
+#' #' Remove empty columns
+#' #'
+#' #' @inheritParams strip_empty_rows
+#' #'
 #'
-#' @return a data frame
-strip_empty_rows <- function(df){
-  df %>%
-    dplyr::filter(dplyr::if_all(.fns = ~!is.na(.)))
-}
-
-#' Remove empty columns
+#' # @example inst/examples/strip_empty_exmaple.R
 #'
-#' @inheritParams strip_empty_rows
+#' strip_empty_cols <- function(df){
+#'   df %>%
+#'     dplyr::select(tidyselect::where(~!all(is.na(.))))
+#' }
 #'
-
-# @example inst/examples/strip_empty_exmaple.R
-
-strip_empty_cols <- function(df){
-  df %>%
-    dplyr::select(tidyselect::where(~!all(is.na(.))))
-}
-
-
-#' Remove both empty rows and columns
 #'
-#' @inheritParams strip_empty_rows
+#' #' Remove both empty rows and columns
+#' #'
+#' #' @inheritParams strip_empty_rows
+#' #'
+#' strip_both <- function(df){
+#'   df %>%
+#'     strip_empty_cols() %>%
+#'     strip_empty_rows()
+#' }
 #'
-strip_both <- function(df){
-  df %>%
-    strip_empty_cols() %>%
-    strip_empty_rows()
-}
-
-
-#' Remove columns or rows containing only NA values
 #'
-#' @param df Data frame.
-#' @param type Character (length 1). Whether to remove empty rows, columns, or both.
+#' #' Remove columns or rows containing only NA values
+#' #'
+#' #' @param df Data frame.
+#' #' @param type Character (length 1). Whether to remove empty rows, columns, or both.
+#' #'
+#' #' @return Data frame with any empty columns/rows removed
+#' #'
+#' strip_empty <- function(df, type = c("both", "rows", "cols")){
 #'
-#' @return Data frame with any empty columns/rows removed
+#'   type <- match.arg(which)
 #'
-strip_empty <- function(df, type = c("both", "rows", "cols")){
-
-  type <- match.arg(which)
-
-  clean_df <- switch (type,
-    'both' = strip_both(),
-    'rows' = strip_empty_rows(),
-    'cols' = strip_empty_cols()
-  )
-
-  return(clean_df)
-
-
-}
+#'   clean_df <- switch (type,
+#'     'both' = strip_both(),
+#'     'rows' = strip_empty_rows(),
+#'     'cols' = strip_empty_cols()
+#'   )
+#'
+#'   return(clean_df)
+#'
+#'
+#' }
--- R/export_plot.R
+++ R/export_plot.R
@@ -3,18 +3,24 @@
 #' Convenient for keeping plots at the ready for presentations, papers, social media, etc.
 #'
 #' @param x ggplot object to save. The file name is automatically populated from the object's name.
-#' @param dir directory in which to save the plots, defaults to 'figs' i.e. a directory named 'figs' located in the top level of current working directory
+#' @param dirs directories in which to save the plots, defaults to 'figs' and
+#'   'presentations/presentation-figs', i.e. a directory named 'figs' located in
+#'   the top level of current working directory and another one specifically for
+#'   SVG versions of R plots used for HTML slide sets.
 #' @param formats character. Image-based file formats to include. Defaults to 'svg', 'png', and 'pdf'.
-#' @param rds Logical. Save in binary .rds format? (Default `TRUE`)
+#' @param rds Logical. Save also in binary .rds format? (Default `TRUE`; useful
+#'   for building R Markdown documents; the plots can be iteratively imported
+#'   into a list and easily called with code rather than individual file paths)
 #' @param ... Other arguments passed to [`cowplot::save_plot()`]
 #'
 #' @return Writes files to disk and prints a message if successful
 #' @export
 #'
-export_plot <- function(x, dir = "figs", formats = c('svg', 'png', 'pdf'), rds = TRUE, ...){
 
-	# browser()
-	# create a fil stem separated by dashes by taking
+export_plot <- function(x, dirs = c('figs', 'presentations/presentation-figs'), formats = c('svg', 'png', 'pdf'), rds = TRUE, ...){
+
+
+	# create a file stem separated by dashes by taking
 	# the object name and substituting
 
 	stem <- substitute(x) %>%
@@ -22,12 +28,20 @@ export_plot <- function(x, dir = "figs", formats = c('svg', 'png', 'pdf'), rds =
 		stringr::str_replace_all("_", "-")
 
 
+	dirs <- match.arg(dirs, choices = c("figs", "presentations/presentation-figs"), several.ok = T)
 
 	formats <- match.arg(formats, choices = c('svg', 'png', 'PNG', 'pdf', 'JPG'), several.ok = T)
 
+	# browser()
+
 # build paths to image versions of figures
 
-visual_paths <- paste0(here::here(dir, formats, paste0(stem, ".", formats)))
+
+	figs_folder_fig_paths <- here::here("figs", formats, glue::glue("{stem}.{formats}"))
+
+	presentation_folder_svg_fig_path <- here::here("presentations", "presentation-figs", glue::glue("{stem}.svg"))
+
+	visual_paths <- c(figs_folder_fig_paths, presentation_folder_svg_fig_path)
 
 # write to all image paths
 
@@ -36,8 +50,6 @@ args <- list(
 	plot = rep(list(x), times = length(visual_paths)))
 
 purrr::pwalk(args, cowplot::save_plot, ...)
-
-# browser()
 
 if(sum(file.exists(visual_paths)) == length(visual_paths)){
 	cat("Files written to disk: \n")
@@ -50,7 +62,7 @@ if(sum(file.exists(visual_paths)) == length(visual_paths)){
 # write  to rds format if not FALSE
 
 	if (rds) {
-		rds_path <- here::here(dir, "rds", paste0(stem, ".rds"))
+		rds_path <- here::here("figs", "rds", paste0(stem, ".rds"))
 		readr::write_rds(x, rds_path)
 		cat("\n", rds_path, "\n")
 	}
