5946f0378aa84d525cc4c8abcfe4ba1397d5aa7f
Evan C. Mascitti <evanmascitti@gmail.com>
2022-08-12 12:54:45 -0400

    new function for finding figure names

---
+++ R/find_paper_figs.R
@@ -0,0 +1,20 @@
+#' Find all figures inserted using `insert_fig()` to allow easy uploading for manuscript submission
+#'
+#' Looks in the `.Rmd` for any calls to this function and extracts the figure file names
+#'
+#' @param file `.Rmd` file to search
+#'
+#' @return character vector of file stems
+#' @export
+#'
+find_paper_figs <- function(file) {
+
+  x <- readLines(
+    con = "./paper/oversizeAttLims-paper-part-1.Rmd"
+  )
+
+  return_vector <- stringr::str_extract(x[stringr::str_detect(x, 'insert_fig')], "(?<=['\"]).*(?=['\"])")
+
+  return(return_vector)
+
+}
